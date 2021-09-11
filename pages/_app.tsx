3b1343e26d4d0457bc5f04b0ff4dd5b6b0646dc0
ukstv <sergey@ukstv.me>
2021-09-11 21:07:15 +0300

    feat: Use Ceramic for querying profile data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CeramicProvider, Networks } from "use-ceramic";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CeramicProvider network={Networks.MAINNET}>
+      <Component {...pageProps} />
+    </CeramicProvider>
+  );
 }
-export default MyApp
+export default MyApp;
