c97189c64b2138d506225a0cdb6e57372375b33a
ukstv <sergey@ukstv.me>
2021-10-27 17:36:09 +0300

    cacao encoded as car

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,12 +10,23 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3 from "web3";
 import * as ipfsHttpClient from "ipfs-http-client";
 import * as uint8arrays from "uint8arrays";
+import { base64url } from "multiformats/bases/base64"
+import { CarReader, CarWriter } from "@ipld/car";
+import { from } from "ix/asynciterable";
 
 const INFURA_TOKEN = process.env.NEXT_PUBLIC_INFURA_TOKEN;
 
 function asIso(timestamp: number): string {
   const date = new Date(timestamp * 1000);
   return date.toISOString().replace(/\.\d+/, "");
+}
+
+async function collect(input: AsyncIterable<Uint8Array>): Promise<Uint8Array> {
+  let acc = new Uint8Array([])
+  for await (let elem of input) {
+    acc = uint8arrays.concat([acc, elem])
+  }
+  return acc
 }
 
 function resourcesList(resources: string[]): string {
@@ -45,7 +56,6 @@ async function connect(): Promise<AuthProvider> {
 
   const chainId = await web3.eth.getChainId();
   const ipfsClient = ipfsHttpClient.create({ url: "http://localhost:5001" });
-  const currentDate = new Date().toISOString().replace(/\.\d+/, "");
 
   const account = accounts[0];
   const cacao = {
@@ -58,6 +68,7 @@ async function connect(): Promise<AuthProvider> {
       uri: "http://localhost:3000/login",
       version: 1,
       nonce: 328917,
+      chainId: chainId,
       iat: Math.floor(new Date().valueOf() / 1000),
       nbf: Math.floor(new Date().valueOf() / 1000),
       exp: Math.floor(new Date().valueOf() / 1000) + 60 * 60, // 1 hour
@@ -81,7 +92,7 @@ async function connect(): Promise<AuthProvider> {
     "\n" +
     `URI: ${cacao.p.uri}\n` +
     `Version: ${cacao.p.version}\n` +
-    `Chain ID: ${chainId}\n` +
+    `Chain ID: ${cacao.p.chainId}\n` +
     `Nonce: ${cacao.p.nonce}\n` +
     `Issued At: ${asIso(cacao.p.iat)}\n` +
     `Expiration Time: ${asIso(cacao.p.exp)}\n` +
@@ -95,7 +106,7 @@ async function connect(): Promise<AuthProvider> {
     signature.replace(/^0x/, ""),
     "base16"
   );
-  console.log('signature', signature.replace(/^0x/, ""))
+  console.log("signature", signature.replace(/^0x/, ""));
   cacao.s.push({
     s: signatureBytes,
   });
@@ -104,6 +115,18 @@ async function connect(): Promise<AuthProvider> {
   console.log("cid", cid.toString());
   const cacaoRestored = await ipfsClient.dag.get(cid);
   console.log("cacao-restored", JSON.stringify(cacaoRestored, null, 4));
+  const block = await ipfsClient.block.get(cid);
+  const { writer, out } = CarWriter.create([cid]);
+  const outPromise = collect(out)
+  await writer.put({
+    cid: cid,
+    bytes: block,
+  });
+  await writer.close();
+  const outResult = await outPromise
+  console.log('output-car-cacao', outResult)
+  const base64urlEncoded = base64url.encode(outResult)
+  console.log('base64url-car-cacao', base64urlEncoded)
 
   return new EthereumAuthProvider(provider, accounts[0]);
 }
